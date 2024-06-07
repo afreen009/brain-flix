c1598da4c085b0f9c80556a87dc8bb8080c6182d
afreen009 <shfafreen@gmail.com>
2024-06-06 21:33:26 -0400

    added video image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
+import VideoDescription from './components/VideoDescription/VideoDescription';
 import './App.scss';
+import videos from './data/videos.json';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
+  const [selectedVideo, setselectedVideo] = useState(videos[0]); 
   return (
     <main className='main'>
-    <Header/>
+      <Header/>
+      <Hero video={selectedVideo}/>
+      <VideoDescription video={selectedVideo}/>
     </main>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import '../Hero/Hero.scss';
+
+const Hero = ({video}) => {
+    return (
+            <section className="hero">
+                <div className="hero__videoContainer">
+                    <video
+                        controls
+                        id="customVideo"
+                        poster={video.image}
+                        className="hero__video--wrapper"
+                        >
+                    </video>
+                </div> 
+            </section>
+    );
+};
+
+export default Hero;
